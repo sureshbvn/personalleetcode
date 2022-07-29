c17e667dbede4197bf19ff67820c3917bd796f5d
sureshbvn <mailsureshbvn@gmail.com>
2022-07-28 20:21:54 -0700

    Create 1228_missing_number_in_airthmetic_progression.py

---
+++ binarysearch/problems/1228_missing_number_in_airthmetic_progression.py
@@ -0,0 +1,66 @@
+# In some array arr, the values were in arithmetic progression: the values arr[i + 1] - arr[i] are all equal for every 0 <= i < arr.length - 1.
+#
+# A value from arr was removed that was not the first or last value in the array.
+#
+# Given arr, return the removed value.
+
+# Example
+# 1:
+#
+# Input: arr = [5, 7, 11, 13]
+# Output: 9
+# Explanation: The
+# previous
+# array
+# was[5, 7, 9, 11, 13].
+# Example
+# 2:
+#
+# Input: arr = [15, 13, 12]
+# Output: 14
+# Explanation: The
+# previous
+# array
+# was[15, 14, 13, 12].
+#
+# Constraints:
+#
+# 3 <= arr.length <= 1000
+# 0 <= arr[i] <= 105
+# The
+# given
+# array is guaranteed
+# to
+# be
+# a
+# valid
+# array.
+
+from typing import List
+
+class Solution:
+    def missingNumber(self, arr: List[int]) -> int:
+
+        arr.sort()
+
+        d = (arr[len(arr) - 1] - arr[0]) // (len(arr))
+
+        low = 0
+        high = len(arr) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            if arr[mid] == (arr[0] + d * mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        # Once the while loop is done, start will point to first bad element
+        # end will point to last good element
+        # so the missing element will be nums[high] + d
+        return arr[high] + d
+
+obj = Solution()
+assert  obj.missingNumber([5,7,11,13]) == 9
+assert  obj.missingNumber([15,13,12]) == 14
