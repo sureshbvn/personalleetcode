bbdc474574a1ca4262c82eaa5d67780a9b0a9f63
sureshbvn <mailsureshbvn@gmail.com>
2022-08-01 20:04:01 -0700

    peak

---
+++ binarysearch/problems/852_peak_index_in_mountain_array.py
@@ -0,0 +1,59 @@
+# An array arr a mountain if the following properties hold:
+#
+# arr.length >= 3
+# There exists some i with 0 < i < arr.length - 1 such that:
+# arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+# arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+# Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] >
+# arr[i + 1] > ... > arr[arr.length - 1].
+#
+# You must solve it in O(log(arr.length)) time complexity.
+#
+# Example
+# 1:
+#
+# Input: arr = [0, 1, 0]
+# Output: 1
+# Example
+# 2:
+#
+# Input: arr = [0, 2, 1, 0]
+# Output: 1
+# Example
+# 3:
+#
+# Input: arr = [0, 10, 5, 2]
+# Output: 1
+#
+# Constraints:
+#
+# 3 <= arr.length <= 105
+# 0 <= arr[i] <= 106
+# arr is guaranteed
+# to
+# be
+# a
+# mountain
+# array.
+
+from typing import List
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        low = 0
+        high = len(arr)-2
+        while low <= high:
+                mid = low + (high - low)//2
+                if arr[mid] < arr[mid+1]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return low
+
+obj = Solution()
+assert obj.peakIndexInMountainArray([0, 1, 0]) == 1
+assert obj.peakIndexInMountainArray([0, 2, 1, 0]) == 1
+assert obj.peakIndexInMountainArray([0, 10, 5, 2]) == 1
+assert obj.peakIndexInMountainArray([1]) == 0
+assert obj.peakIndexInMountainArray([1,2]) == 1
+assert obj.peakIndexInMountainArray([2,1]) == 0
