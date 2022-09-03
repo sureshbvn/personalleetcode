31b8b2dea80afec563ba9cecbf72b3de823903a9
sureshbvn <mailsureshbvn@gmail.com>
2022-09-03 01:05:07 +0000

    Added DFS solution

--- graphs/problems/traversal/262_is_graph_valid_tree.py
+++ graphs/problems/traversal/262_is_graph_valid_tree.py
@@ -17,7 +17,7 @@ class Solution:
             adjList[v].append(u)
             
         
-        # Step2. Write an inner function called bfs. This is an iterative function.
+        # Step2(a). Write an inner function called bfs. This is an iterative function.
         # The function returns a boolean to indicate if a cycle is found.
         def bfs(source, visitedSet) -> bool:
             
@@ -40,13 +40,37 @@ class Solution:
             return False
         
         
+        # Step 2(b)
+        # Dfs on the graph. Returns true if there is cycle. 
+        def dfs(source, parent, visitedSet) -> bool :
+            
+            for vertex in adjList[source]:
+                if vertex not in visitedSet:
+                    visitedSet.add(vertex)
+                    if dfs(vertex, source, visitedSet):
+                        return True         
+                else:
+                    if vertex != parent:
+                        return True
+            
+            return False
+                        
+        
         # Step3: The actual logic.
         visitedSet = set()
         connected = 0
         for node in range(0, n):
             if node not in visitedSet:
                 # If atleast one connected component returns cycle, return true.
-                if bfs(node, visitedSet):
+                #if bfs(node, visitedSet):
+                    #return False
+                    
+                # In case of dfs add the node to visited. We can do this for BFS too.
+                visitedSet.add(node)
+                
+                # With DFS we can just send parent to the recursion. This avoids have a parent Map that
+                # we had in BFS iterative solution. 
+                if dfs(node, None, visitedSet):
                     return False
                 
                 # The number of connected components should be strictly one for the graph to be a valid tree.
